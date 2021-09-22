baa54a4cbe538bf2862d6f377e274d468a54dfdb
Kelvin <chuakid@gmail.com>
2021-09-23 00:05:52 +0800

    Refactored scripts Added passthrough copy for styles Moved everything into src folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,15 @@
-module.exports = function (eleventyConfig) {
+module.exports = function (config) {
     // Output directory: _site
     // Copy static folder
-    eleventyConfig.addPassthroughCopy("static");
+    config.addPassthroughCopy("static");
+    config.addPassthroughCopy({
+        'src/_includes/assets/css/style.css': './style.css'
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "_site"
+        }
+    };
 };
