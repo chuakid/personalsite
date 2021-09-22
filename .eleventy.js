79db51a689f6f3dca97af7daf74150198dc065dd
Kelvin <chuakid@gmail.com>
2021-09-23 00:10:27 +0800

    Fixed static folder passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (config) {
     // Output directory: _site
     // Copy static folder
-    config.addPassthroughCopy("static");
+    config.addPassthroughCopy("src/static");
     config.addPassthroughCopy({
         'src/_includes/assets/css/style.css': './style.css'
     });
