66e40cdacc86c1e0156039dec1191078321437fc
Kelvin <chuakid@gmail.com>
2021-09-23 02:38:48 +0800

    Changed design of site Added feathericons

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,7 @@ module.exports = function (config) {
     // Output directory: _site
     // Copy static folder
     config.addPassthroughCopy("src/static");
-    config.addPassthroughCopy({
-        'src/_includes/assets/css/style.css': './style.css'
-    });
+    config.addPassthroughCopy({'src/_includes/assets/': "/assets/"});
 
     return {
         dir: {
