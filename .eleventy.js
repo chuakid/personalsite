703bc8ef635b22ae004f66aac70a26b25c26639a
Kelvin <chuakid@gmail.com>
2021-09-23 12:20:04 +0800

    Fixed tailwind's purging

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ module.exports = function (config) {
     // Output directory: _site
     // Copy static folder
     config.addPassthroughCopy("src/static");
-    config.addPassthroughCopy({'src/_includes/assets/': "/assets/"});
 
     return {
         dir: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['_site/**/*.html'],
+  purge: ['./_site/**/*.html', './src/**/*.njk'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
