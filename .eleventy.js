c3458cd0dedf59c9832fce5f27ef6e92fa81b9dc
Kelvin <chuakid@gmail.com>
2021-09-24 21:17:09 +0800

    Added filter for svg

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,18 @@ module.exports = function (config) {
     // Copy static folder
     config.addPassthroughCopy("src/static");
 
+    //For printing SVGs
+    const fs = require("fs");
+
+    config.addFilter('printFileContents', function (filePath) {
+        const fileContents = fs.readFileSync(filePath, (err, data) => {
+            if (err) throw err;
+            return data;
+        });
+
+        return fileContents;
+    });
+
     return {
         dir: {
             input: "src",
