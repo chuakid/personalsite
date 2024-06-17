19c016491f0dcdfd0817e98671d66dc6e99625f7
Kelvin <chuakid@gmail.com>
2024-06-17 18:23:28 +0800

    clone of eleventy ver

---
+++ src/config.ts
@@ -0,0 +1,18 @@
+export const config = {
+    "name": "Kelvin Chua",
+    "info": {
+        "about": "I graduated from the National University of Singapore with a bachelor's in Computer Science. I am currently working as a Site Reliability Engineer at Shopee",
+        "github": "https://github.com/chuakid",
+        "linkedin": "https://www.linkedin.com/in/kelvin-chua-b18187154/",
+        "email": "chuakid@gmail.com"
+    },
+    "skills": [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "TailwindCSS",
+        "React",
+        "Vue",
+        "NodeJS"
+    ]
+}
---
+++ src/content/config.ts
@@ -0,0 +1,30 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+// 2. Define your collection(s)
+const experiences = defineCollection({
+    type: 'data',
+    schema: z.object({
+        jobTitle: z.string(),
+        company: z.string(),
+        description: z.string(),
+        date: z.string(),
+        link: z.string().url().optional(),
+        tags: z.array(z.string())
+    })
+ });
+const projects = defineCollection({
+    type: 'data',
+    schema: z.object({
+        title: z.string(),
+        tags: z.array(z.string()),
+        description: z.string(),
+        source: z.string().url().optional(),
+        link: z.string().url().optional()
+    })
+ });
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  experiences,
+  projects
+};
